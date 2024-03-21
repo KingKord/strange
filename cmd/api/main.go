bd0419c8f60b8c032e59a3b3f7ab5ecdb026ebc4
gorbatykh <gorbatykh.bbusiness@gmail.com>
2024-03-21 19:41:25 +0300

    project structure update

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"fmt"
+	_ "github.com/KingKord/strange/docs"
+
 	"github.com/KingKord/strange/internal/handlers"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"log"
 	"net/http"
 )
 
+const port = "3333"
+
+// @title strange API
+// @version 1.0
+
+// @host localhost:3333
+// @BasePath /api/v1/
+
 func main() {
 	newHandlers := handlers.NewHandlers()
 
-	log.Println("Starting service on port 3333")
-	err := http.ListenAndServe(":3333", getRoutes(newHandlers))
+	log.Println(fmt.Sprintf("Starting service on port %s", port))
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), getRoutes(newHandlers))
 	if err != nil {
 		log.Fatalf("cannot start the application due: %s", err)
 	}
@@ -28,12 +40,18 @@ func getRoutes(handler handlers.Handlers) http.Handler {
 		AllowCredentials: true,
 		MaxAge:           300,
 	}))
+	mux.Get("/swagger/*", httpSwagger.Handler(
+		httpSwagger.URL(fmt.Sprintf("http://localhost:%s/swagger/doc.json", port)), //The url pointing to API definition
+	))
 
-	mux.Get("/", handler.Root)
+	mux.Route("/api/v1", func(mux chi.Router) {
+		mux.Get("/", handler.Root)
 
-	mux.Route("/schedule", func(router chi.Router) {
-		router.Get("/day", handler.DaySchedule)
-		router.Post("/reserve", handler.AssignMeet)
+		mux.Route("/schedule", func(router chi.Router) {
+			router.Get("/day", handler.DaySchedule)
+			router.Post("/reserve", handler.AssignMeet)
+		})
+
 	})
 
 	return mux
--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"github.com/KingKord/strange/internal/helpers"
 	"log"
 	"net/http"
 )
@@ -12,8 +13,26 @@ func NewHandlers() Handlers {
 	return Handlers{}
 }
 
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+// Root is an API endpoint that register a user and returns a JSON response.
+// @Summary Get root endpoint
+// @Description Get the root endpoint
+// @Tags Root
+// @Accept json
+// @Produce json
+// @Success 200 {object} jsonResponse
+// @Router / [get]
 func (h Handlers) Root(w http.ResponseWriter, r *http.Request) {
 	log.Println("got root successfully!")
+	_ = helpers.WriteJSON(w, http.StatusOK, jsonResponse{
+		Error:   false,
+		Message: "successfully got Root",
+	})
 }
 
 func (h Handlers) DaySchedule(w http.ResponseWriter, r *http.Request) {
